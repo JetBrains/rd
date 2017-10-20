b750a8bb8c49022ad56e95d5977db2857244b9f5
Sergey Shkredov <sergey.shkredov@jetbrains.com>
2017-10-20 18:18:49 +0300

    some changes

--- rider-framework/Src/com/jetbrains/rider/framework/base/RdDelegateBase.kt
+++
@@ -1,3 +0,0 @@
-package com.jetbrains.rider.framework.base
-
-abstract class RdDelegateBase<out T : IRdReactive>(val delegate: T) : IRdReactive by delegate
--- rider-util/Src/com/jetbrains/rider/util/time/InfiniteDuration.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.rider.util.time
-
-import java.time.Duration
-import java.time.Instant
-
-val InfiniteDuration = Duration.ofSeconds(1000000000)
