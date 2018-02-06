a4ca9d0ed20d770669404c2a1494b3c0cfbfbaf5
Dmitry.Ivanov <korifey@gmail.com>
2018-02-06 16:17:22 +0300

    Removed old sources before committing new one

--- rd-core/src/main/kotlin/jetbrains/rd/util/collections/Stack.kt
+++
@@ -1,7 +0,0 @@
-package com.jetbrains.rider.util.collections
-
-interface Stack<T> : Collection<T> {
-    fun pop(): T
-    fun push(value: T)
-    fun peek(): T?
-}
--- rd-gen/src/main/kotlin/rider/generator/nova/util/EnvUtil.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.rider.generator.nova.util
-
-
-fun syspropertyOrEmpty(name: String) : String = System.getProperty(name) ?: ""
-
-
