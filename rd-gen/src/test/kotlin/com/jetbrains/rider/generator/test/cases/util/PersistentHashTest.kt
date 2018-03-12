cf19e0c49745c2cda8b43384a338c7afcd92a34f
Dmitry.Ivanov <korifey@gmail.com>
2018-03-12 20:28:26 +0300

    Moved PersistentHash and its test + changed TestNg -> Junit

--- rd-gen/src/test/kotlin/com/jetbrains/rider/generator/test/cases/util/PersistentHashTest.kt
+++ rd-gen/src/test/kotlin/com/jetbrains/rider/generator/test/cases/util/PersistentHashTest.kt
@@ -1,10 +1,10 @@
-package com.jetbrains.rider.util.test.cases
+package com.jetbrains.rider.generator.test.cases.util
 
 import com.jetbrains.rider.util.hash.PersistentHash
-import org.testng.Assert
-import org.testng.annotations.AfterTest
-import org.testng.annotations.BeforeTest
-import org.testng.annotations.Test
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -16,12 +16,12 @@ class PersistentHashTest {
 
     var file : Path = Paths.get("fake")
 
-    @BeforeTest
+    @Before
     fun setUp() {
         file  = Files.createTempFile("PersistentHash", ".txt")
     }
 
-    @AfterTest
+    @After
     fun tearDown() {
         file.toFile().delete()
     }
