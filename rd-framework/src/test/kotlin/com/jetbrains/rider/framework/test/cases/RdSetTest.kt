fcf03ec35350deaeb112e27cd0b730c9b3bf3722
operasfantom <operasfantom@gmail.com>
2018-08-03 12:42:12 +0300

    Update test to check order

--- rd-framework/src/test/kotlin/com/jetbrains/rider/framework/test/cases/RdSetTest.kt
+++ rd-framework/src/test/kotlin/com/jetbrains/rider/framework/test/cases/RdSetTest.kt
@@ -15,11 +15,12 @@ class RdSetTest : RdFrameworkTestBase() {
         val clientSet = RdSet<Int>().static(id)
 
         val log = arrayListOf<Int>()
-        serverSet.advise(serverLifetimeDef.lifetime) {kind, v -> log.add(if (kind == AddRemove.Add) v else -v)}
+        serverSet.advise(serverLifetimeDef.lifetime) { kind, v -> log.add(if (kind == AddRemove.Add) v else -v) }
 
-        clientSet.add(1)
-        clientSet.add(1)
         clientSet.add(2)
+        clientSet.add(0)
+        clientSet.add(1)
+        clientSet.add(8)
         clientSet.add(3)
 
 
@@ -27,20 +28,20 @@ class RdSetTest : RdFrameworkTestBase() {
 
         serverProtocol.bindStatic(serverSet, "top")
         clientProtocol.bindStatic(clientSet, "top")
-        assertEquals(listOf(1, 2, 3), log)
+        assertEquals(listOf(2, 0, 1, 8, 3), log)
 
-        clientSet.retainAll(listOf(1, 2))
-        assertEquals(listOf(1, 2, 3, -3), log)
+        clientSet.retainAll(listOf(8, 3, 0))
+        assertEquals(listOf(2, 0, 1, 8, 3, -2, -1), log)
 
-        serverSet.remove(3)
-        clientSet.remove(3)
-        assertEquals(listOf(1, 2, 3, -3), log)
+        serverSet.remove(1)
+        clientSet.remove(1)
+        assertEquals(listOf(2, 0, 1, 8, 3, -2, -1), log)
 
-        clientSet.removeAll { it == 1 }
-        assertEquals(listOf(1, 2, 3, -3, -1), log)
+        clientSet.removeAll { it == 3 }
+        assertEquals(listOf(2, 0, 1, 8, 3, -2, -1, -3), log)
 
 
         clientSet.clear()
-        assertEquals(listOf(1, 2, 3, -3, -1, -2), log)
+        assertEquals(listOf(2, 0, 1, 8, 3, -2, -1, -3, -0, -8), log)
     }
 }
