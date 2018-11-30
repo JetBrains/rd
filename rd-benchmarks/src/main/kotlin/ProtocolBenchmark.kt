ba0c501dc9c8e785bedc39e658ea1333862b96d9
Dmitry Jemerov <yole@jetbrains.com>
2018-11-30 15:05:18 +0100

    Cleanup benchmarks code

--- rd-benchmarks/src/main/kotlin/ProtocolBenchmark.kt
+++ rd-benchmarks/src/main/kotlin/ProtocolBenchmark.kt
@@ -1,39 +1,34 @@
 package com.jetbrans.rd.benchmarks
 
 import com.jetbrains.rd.benchmarks.model.BenchmarkRootModel
-import com.jetbrains.rider.framework.Identities
-import com.jetbrains.rider.framework.Protocol
-import com.jetbrains.rider.framework.Serializers
-import com.jetbrains.rider.framework.SocketWire
+import com.jetbrains.rider.framework.*
+import com.jetbrains.rider.util.lifetime.Lifetime
 import com.jetbrains.rider.util.lifetime.LifetimeDefinition
-import com.jetbrains.rider.util.threading.SynchronousScheduler
+import com.jetbrains.rider.util.reactive.IScheduler
 import java.util.concurrent.CountDownLatch
 
+class ProtocolSide(wireCallback: (Lifetime, IScheduler) -> IWire) {
+    val lifetime = LifetimeDefinition()
+    private val serializers = Serializers()
+    private val identities = Identities()
+    private val scheduler = BenchmarkScheduler()
+    val wire = wireCallback(lifetime.lifetime, scheduler)
+    private val protocol = Protocol(serializers, identities, scheduler, wire)
+
+    val model = BenchmarkRootModel.create(lifetime, protocol)
+}
+
 fun main() {
-    val serverLifetime = LifetimeDefinition()
-    val serverSerializers = Serializers()
-    val serverIdentities = Identities()
-    val serverScheduler = BenchmarkScheduler()
-    val serverWire = SocketWire.Server(serverLifetime.lifetime, serverScheduler, port = null, optId = "Server")
-    val serverProtocol = Protocol(serverSerializers, serverIdentities, serverScheduler, serverWire)
-
-    val clientLifetime = LifetimeDefinition()
-    val clientSerializers = Serializers()
-    val clientIdentities = Identities()
-    val clientScheduler = BenchmarkScheduler()
-    val clientWire = SocketWire.Client(clientLifetime.lifetime, clientScheduler, serverWire.port, optId = "Client")
-    val clientProtocol = Protocol(clientSerializers, clientIdentities, clientScheduler, clientWire)
-
-    val serverModel = BenchmarkRootModel.create(serverLifetime, serverProtocol)
-    val clientModel = BenchmarkRootModel.create(clientLifetime, clientProtocol)
+    val server = ProtocolSide { lifetime, scheduler -> SocketWire.Server(lifetime, scheduler, port = null, optId = "Server") }
+    val client = ProtocolSide { lifetime, scheduler -> SocketWire.Client(lifetime, scheduler, port = server.wire.serverPort, optId = "Server") }
 
     val count = 1000
     val latch = CountDownLatch(1)
-    clientModel.count.view(clientLifetime) { _, value -> if (value == count) latch.countDown() }
+    client.model.count.view(client.lifetime) { _, value -> if (value == count) latch.countDown() }
 
     val startTime = System.nanoTime()
     for (i in 1..count) {
-        serverModel.count.set(i)
+        server.model.count.set(i)
     }
     latch.await()
     val endTime = System.nanoTime()
