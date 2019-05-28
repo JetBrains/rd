952abafb0f46040948b6944c86ce72b0afd7c2dd
Dmitry.Ivanov <korifey@gmail.com>
2019-05-28 17:28:59 +0300

    Tests for Rd.Core

---
+++ rd-net/Test.RdCore/Collections/Synchronized/TestSynchronizedDictionary.cs
@@ -0,0 +1,20 @@
+using JetBrains.Collections.Synchronized;
+using NUnit.Framework;
+
+namespace Test.RdCore.Collections.Synchronized
+{
+    [TestFixture]
+    public class TestSynchronizedDictionary
+    {
+        public void TestLiveFiltering()
+        {
+            var dict = new SynchronizedDictionary<int, string>
+            {
+                {1, "1"},
+                {2, "2"}
+            };
+            
+            
+        }
+    }
+}
