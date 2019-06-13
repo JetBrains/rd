4ce7d5635b3db3c8a850f6665dfbd9219d368717
operasfantom <operasfantom@gmail.com>
2019-06-13 21:53:04 +0300

    task Refactoring

---
+++ buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/tasks/RunExecTask.kt
@@ -0,0 +1,30 @@
+package com.jetbrains.rd.gradle.tasks;
+
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.tasks.Exec;
+import org.gradle.api.tasks.TaskAction
+
+open class RunExecTask : Exec() {
+    lateinit var execPath: String
+
+    init {
+        group = "exec"
+        outputs.upToDateWhen { false }
+    }
+
+    @TaskAction
+    override fun exec() {
+        println("EXEC")
+        println(execPath)
+        println(args)
+
+        when {
+            Os.isFamily(Os.FAMILY_WINDOWS) -> commandLine = listOf("cmd", "/c", "$execPath.exe") + listOf(args)
+            Os.isFamily(Os.FAMILY_UNIX) -> commandLine = listOf("./$execPath")
+            Os.isFamily(Os.FAMILY_MAC) -> {
+                //todo
+            }
+        }
+        super.exec()
+    }
+}
