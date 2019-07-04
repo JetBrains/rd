569a3ead71fc3dbcd5e2650e0eb4152ada53b4f8
operasfantom <operasfantom@gmail.com>
2019-07-04 11:00:08 +0300

    remove googletest submodule

--- rd-cpp/buildtest.cmd
+++ rd-cpp/buildtest.cmd
@@ -15,9 +15,10 @@ exit 0
 :CMDSCRIPT
 @echo on
 pushd "%~dp0"
+call get_dependencies.cmd
 mkdir build
 pushd build
-cmake -DENABLE_TESTS_OPTION:BOOL=OFF -G "Visual Studio 16 2019" ..
-cmake --build . --config Release -j 4
-cmake -DENABLE_TESTS_OPTION:BOOL=OFF -DBUILD_TYPE=Release -P cmake_install.cmake
+cmake -DENABLE_TESTS_OPTION:BOOL=ON -G "Visual Studio 16 2019" ..
+cmake --build . --config Release -j 4 
+cmake -DENABLE_TESTS_OPTION:BOOL=ON -DBUILD_TYPE=Release -P cmake_install.cmake
 popd
