1febc6ca0a78dd278f56d23f445b3cbfe144c207
Dmitry.Ivanov <korifey@gmail.com>
2019-09-05 20:33:44 +0300

    Fixed 50 ms warmup on unity Appdomain startup: https://youtrack.jetbrains.com/issue/RIDER-32492

--- rd-net/Lifetimes/Util/CastTo.cs
+++ rd-net/Lifetimes/Util/CastTo.cs
@@ -1,16 +1,45 @@
 using System;
 using System.Linq.Expressions;
+using JetBrains.Annotations;
 
 namespace JetBrains.Util.Util
 {
   /// <summary>
   /// !!! Use it with caution. Main purpose is enum to/from int casting without boxing !!!
-  /// /// https://stackoverflow.com/questions/1189144/c-sharp-non-boxing-conversion-of-generic-enum-to-int/23391746#23391746
   /// </summary>
   public static class CastTo<TTo>
   {
-    /// <summary>    
-    /// Casts <see cref="TFrom"/> to TTo  without boxing for value types. Useful in generic methods.    
+    /// <summary>
+    /// Caution, for experienced coders only!!! TFrom and TTo should have same field layout (and better be POD structs).
+    /// The same as LINQ Expression based <see cref="From{TFrom}"/> but without any compiler checks and warm up penalty.
+    /// </summary>
+    /// <param name="original"></param>
+    /// <typeparam name="TFrom"></typeparam>
+    /// <returns>(TTo)original</returns>
+    [PublicAPI] internal static unsafe TTo ReinterpretCastFrom<TFrom>(TFrom original)
+    {
+      TTo placeholder = default(TTo);
+      var trPlaceholder = __makeref(placeholder);
+      var trOriginal = __makeref(original);
+      
+      // Sick shit. The original reference is a pointer to a struct which has a pointer to the target in its
+      // first field (“placeholder” stack var in our case), for a fixed-address thing that's about all it got.
+      // We replace this pointer with an equivalent fixed pointer to our “original” stack var,
+      // whose target value is then used for extracting the value
+      *(void**)&trPlaceholder = *(void**)&trOriginal; 
+      return __refvalue(trPlaceholder, TTo);
+
+      // NOTE: if this stops working, backup plan #1 is to have an array of type TTo[] in some generic struct,
+      // to get hold of it with a pin-handle and write the raw data into it (or two arrays as needed)
+      // NOTE: backup plan #2: use SafeBuffer::PtrToStructureNative, but it's not easy to call from outside code
+      // (have to make up a shared SafeBuffer and do Write<T> / Read<T> calls with it, for POD structs they are efficient enough)
+    }
+    
+    
+    /// <summary>
+    /// /// https://stackoverflow.com/questions/1189144/c-sharp-non-boxing-conversion-of-generic-enum-to-int/23391746#23391746
+    /// Casts <see cref="TFrom"/> to TTo  without boxing for value types. Useful in generic methods.
+    /// The only problem is that this method requires around 50ms on startup to warm up. So for real hardcore see <see cref="ReinterpretCastFrom{TFrom}"/>  
     /// </summary>
     /// <typeparam name="TFrom">Source type to cast from. Usually a generic type.</typeparam>
     public static TTo From<TFrom>(TFrom s)
---
+++ rd-net/Test.Lifetimes/Utils/CastToTest.cs
@@ -0,0 +1,62 @@
+using System;
+using JetBrains.Collections.Viewable;
+using JetBrains.Util.Util;
+using NUnit.Framework;
+
+namespace Test.Lifetimes.Utils
+{
+    public class CastToTest
+    {
+        [Test]
+        public void TestReinterpretCastValueTypes()
+        {
+            int updateOrdinal = (int) AddUpdateRemove.Update;
+            int removeOrdinal = (int) AddUpdateRemove.Remove;
+            
+            Assert.AreEqual(AddUpdateRemove.Update, CastTo<AddUpdateRemove>.ReinterpretCastFrom(updateOrdinal));
+            Assert.AreEqual(AddUpdateRemove.Remove, CastTo<AddUpdateRemove>.ReinterpretCastFrom(removeOrdinal));
+        }
+        
+        
+        [Test]
+        public void TestCastValueTypes()
+        {
+            int updateOrdinal = (int) AddUpdateRemove.Update;
+            int removeOrdinal = (int) AddUpdateRemove.Remove;
+            
+            Assert.AreEqual(AddUpdateRemove.Update, CastTo<AddUpdateRemove>.From(updateOrdinal));
+            Assert.AreEqual(AddUpdateRemove.Remove, CastTo<AddUpdateRemove>.From(removeOrdinal));
+        }
+        
+        
+#if !NET35 
+        
+        [Test]
+        public void TestReinterpretCastReferenceTypes()
+        {
+            var from = new Tuple<ulong>(0x0123456789abcdef);
+            var to = new Tuple<uint,uint>(0x89abcdef,0x01234567);
+
+            var reinterpreted = CastTo<Tuple<uint,uint>>.ReinterpretCastFrom(@from);
+
+            Assert.AreEqual(to.Item1, reinterpreted.Item1);
+            Assert.AreEqual(to.Item2, reinterpreted.Item2);
+            
+            Assert.AreNotEqual(to, reinterpreted);
+            Assert.AreEqual(from, reinterpreted);
+        }
+        
+        
+        [Test]
+        public void TestCastReferenceTypes()
+        {
+            var from = new Tuple<ulong>(0x0123456789abcdef);
+            var to = new Tuple<uint,uint>(0x89abcdef,0x01234567);
+
+            Assert.Throws<TypeInitializationException>(() => CastTo<Tuple<uint,uint>>.From(@from));
+        }
+        
+        
+#endif
+    }
+}
