be70220e4197aca20925980e70fa57b4f148250c
dmitry.ivanov <korifey@gmail.com>
2019-09-09 20:41:08 +0300

    Remove dependency to package JetBrains.Annotations

---
+++ rd-net/Lifetimes/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Test.Lifetimes")]
+[assembly: InternalsVisibleTo("Test.RdFramework")]
+[assembly: InternalsVisibleTo("JetBrains.RdFramework")]
