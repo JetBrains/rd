471ac5c87883332b7c804d54e4a5e43b87591c14
epeshk <evgeny.peshkov@jetbrains.com>
2019-09-17 12:47:24 +0300

    Removed magic cast due to it is not working on Mono.

--- rd-net/Test.Lifetimes/Utils/CastToTest.cs
+++ rd-net/Test.Lifetimes/Utils/CastToTest.cs
@@ -9,17 +9,6 @@ namespace Test.Lifetimes.Utils
     public class CastToTest
     {
         [Test]
-        public void TestReinterpretCastValueTypes()
-        {
-            int updateOrdinal = (int) AddUpdateRemove.Update;
-            int removeOrdinal = (int) AddUpdateRemove.Remove;
-            
-            Assert.AreEqual(AddUpdateRemove.Update, CastTo<AddUpdateRemove>.ReinterpretFrom(updateOrdinal));
-            Assert.AreEqual(AddUpdateRemove.Remove, CastTo<AddUpdateRemove>.ReinterpretFrom(removeOrdinal));
-        }
-        
-        
-        [Test]
         public void TestCastValueTypes()
         {
             int updateOrdinal = (int) AddUpdateRemove.Update;
@@ -44,23 +33,7 @@ namespace Test.Lifetimes.Utils
         }
         
         
-#if !NET35 
-        
-        [Test]
-        public void TestReinterpretCastReferenceTypes()
-        {
-            var from = new Tuple<ulong>(0x0123456789abcdef);
-            var to = new Tuple<uint,uint>(0x89abcdef,0x01234567);
-
-            var reinterpreted = CastTo<Tuple<uint,uint>>.ReinterpretFrom(@from);
-
-            Assert.AreEqual(to.Item1, reinterpreted.Item1);
-            Assert.AreEqual(to.Item2, reinterpreted.Item2);
-            
-            Assert.AreNotEqual(to, reinterpreted);
-            Assert.AreEqual(from, reinterpreted);
-        }
-        
+#if !NET35
         
         [Test]
         public void TestCastReferenceTypes()
