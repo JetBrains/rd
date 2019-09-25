bf2d9d02874f4daede689b51fd52475920c6f0dd
operasfantom <operasfantom@gmail.com>
2019-09-25 20:11:24 +0300

    remove remaining .gradle files

---
+++ rd-net/build.gradle.kts
@@ -0,0 +1,20 @@
+import com.jetbrains.rd.gradle.tasks.DotnetRunTask
+import com.jetbrains.rd.gradle.tasks.RunScriptTask
+
+
+/*tasks {
+    val build by creating(RunScriptTask::class) {
+        execPath = "build.cmd"
+    }
+
+    val clean by creating(Exec::class) {
+        executable = "dotnet"
+        args = listOf("clean")
+    }
+
+    val CrossTestCsClientAllEntities by creating(DotnetRunTask::class)
+
+    val CrossTestCsClientBigBuffer by creating(DotnetRunTask::class)
+
+    val CrossTestCsClientRdCall by creating(DotnetRunTask::class)
+}*/
