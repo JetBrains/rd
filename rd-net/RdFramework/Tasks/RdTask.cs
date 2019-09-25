df82b51f64269ce7c5fa7341c4df133d5b1caad5
Dmitry.Ivanov <korifey@gmail.com>
2019-09-25 19:55:03 +0300

    ++rdtask revised

--- rd-net/RdFramework/Tasks/RdTask.cs
+++ rd-net/RdFramework/Tasks/RdTask.cs
@@ -13,6 +13,8 @@ using JetBrains.Rd.Base;
 using JetBrains.Rd.Impl;
 using JetBrains.Rd.Util;
 using JetBrains.Serialization;
+using JetBrains.Threading;
+
 #if !NET35
 
 #endif
@@ -20,31 +22,19 @@ using JetBrains.Serialization;
 
 namespace JetBrains.Rd.Tasks
 {
+  
 
-
-  public interface IRdTask
-  {
-    
-  }
-
-  public interface IRdTask<T> : IRdTask
+  [PublicAPI] public interface IRdTask<T>
   {
     IReadonlyProperty<RdTaskResult<T>> Result { get; }
   }
 
   public static class RdTasksEx
   {
-    public static bool IsSuceedeed<T>(this IRdTask<T> task)
-    {
-      return task.Result.HasValue() && task.Result.Value.Status == RdTaskStatus.Success;
-    }
+    public static bool IsSuceedeed<T>(this IRdTask<T> task) => task.Result.HasValue() && task.Result.Value.Status == RdTaskStatus.Success;
 
-    public static bool Wait<T>(this IRdTask<T> task, TimeSpan timeout)
-    {
-      var evt = new ManualResetEvent(false);
-      task.Result.Advise(Lifetime.Eternal, _ => evt.Set());
-      return evt.WaitOne((int)timeout.TotalMilliseconds);
-    }
+    public static bool Wait<T>(this IRdTask<T> task, TimeSpan timeout) => SpinWaitEx.SpinUntil(Lifetime.Eternal, timeout, () => task.Result.HasValue());
+    
 #if !NET35
     public static RdTask<T> ToRdTask<T>(this Task<T> task, CancellationToken token, TaskContinuationOptions continuationOptions = TaskContinuationOptions.None, TaskScheduler scheduler = null)
     {
@@ -132,101 +122,6 @@ namespace JetBrains.Rd.Tasks
     Success,
     Canceled,
     Faulted
-  }
-
-  public sealed class RdTaskResult<T> : IPrintable
-  {
-    private readonly RdTaskStatus myStatus;
-    private readonly T myResult;
-    private readonly RdFault myError;
-
-    internal RdTaskResult(RdTaskStatus status, T result, RdFault error)
-    {
-      myStatus = status;
-      myResult = result;
-      myError = error;
-    }
-
-    public RdTaskStatus Status { get { return myStatus; } }
-    public T Result { get { return myResult; } }
-    public RdFault Error { get { return myError; } }
-
-
-    internal static RdTaskResult<T> Success(T result)
-    {
-      return new RdTaskResult<T>(RdTaskStatus.Success, result, null);
-    }
-    internal static RdTaskResult<T> Cancelled()
-    {
-      return new RdTaskResult<T>(RdTaskStatus.Canceled, default(T), null);
-    }
-    internal static RdTaskResult<T> Faulted(Exception exception)
-    {
-      return new RdTaskResult<T>(RdTaskStatus.Faulted, default(T), exception as RdFault ?? new RdFault(exception));
-    }
-
-    public T Unwrap()
-    {
-      switch (Status)
-      {
-        case RdTaskStatus.Success:   return Result;
-        case RdTaskStatus.Canceled:  throw new OperationCanceledException();
-        case RdTaskStatus.Faulted:   throw Error;
-        default:
-          throw new ArgumentOutOfRangeException(Status + "");
-      }
-    }
-
-
-    public static RdTaskResult<T> Read(CtxReadDelegate<T> readDelegate, SerializationCtx ctx, UnsafeReader reader)
-    {
-      var status = (RdTaskStatus)reader.ReadInt();
-
-      switch (status)
-      {
-        case RdTaskStatus.Success:   return Success(readDelegate(ctx, reader));
-        case RdTaskStatus.Canceled:  return Cancelled();
-        case RdTaskStatus.Faulted:   return Faulted(RdFault.Read(ctx, reader));
-        default:
-          throw new ArgumentOutOfRangeException(status + "");
-      }
-    }
-
-    public static void Write(CtxWriteDelegate<T> writeDelegate, SerializationCtx ctx, UnsafeWriter writer, RdTaskResult<T> value)
-    {
-      writer.Write((int)value.Status);
-
-      switch (value.Status)
-      {
-        case RdTaskStatus.Success:
-          writeDelegate(ctx, writer, value.Result);
-          break;
-        case RdTaskStatus.Canceled:
-          break;
-        case RdTaskStatus.Faulted:
-          RdFault.Write(ctx, writer, value.Error);
-          break;
-        default:
-          throw new ArgumentOutOfRangeException();
-      }
-    }
-
-
-    public void Print(PrettyPrinter printer)
-    {
-      printer.Print(myStatus.ToString());
-      if (myStatus != RdTaskStatus.Canceled)
-      {
-        printer.Print(" :: ");
-        if (myStatus == RdTaskStatus.Success)
-        {
-          Result.PrintEx(printer);
-        } else if (myStatus == RdTaskStatus.Faulted)
-        {
-          Error.PrintEx(printer);
-        }
-      }
-    }
   }
 
 
