b14b53d113fda9ffb2ceef4b31eed87b136b1e48
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2019-10-17 18:09:07 +0200

    Fix teamcity build with older C# compiler

--- rd-net/Test.RdFramework/Reflection/ProxyGeneratorCornerCasesTests.cs
+++ rd-net/Test.RdFramework/Reflection/ProxyGeneratorCornerCasesTests.cs
@@ -13,7 +13,7 @@ namespace Test.RdFramework.Reflection
     [RdRpc] public interface IInvalid3 { void M<T>(out int refx); }
     [RdRpc] public interface IInvalid4<T> { void M(out int refx); }
     [RdRpc] public interface IInvalid5 { ref int M(); }
-    [RdRpc] public interface IInvalid6 { int M(in int x); }
+    //[RdRpc] public interface IInvalid6 { int M(in int x); }
     [RdRpc] public interface IInvalid7 { event Action<string> Event; }
     [RdRpc] public interface IInvalid8 { string X { set; } }
     [RdRpc] public interface IInvalid9 { string X { get; set; } }
@@ -30,7 +30,7 @@ namespace Test.RdFramework.Reflection
     [Test] public void TestInvalid3() { Assert.Throws<ArgumentException>(() => { myProxyGenerator.CreateType<IInvalid3>(); }); }
     [Test] public void TestInvalid4() { Assert.Throws<ArgumentException>(() => { myProxyGenerator.CreateType<IInvalid4<int>>(); }); }
     [Test] public void TestInvalid5() { Assert.Throws<ArgumentException>(() => { myProxyGenerator.CreateType<IInvalid5>(); }); }
-    [Test] public void TestInvalid6() { Assert.Throws<ArgumentException>(() => { myProxyGenerator.CreateType<IInvalid6>(); }); }
+    //[Test] public void TestInvalid6() { Assert.Throws<ArgumentException>(() => { myProxyGenerator.CreateType<IInvalid6>(); }); }
     [Test] public void TestInvalid7() { Assert.Throws<NotSupportedException>(() => { myProxyGenerator.CreateType<IInvalid7>(); }); }
     [Test] public void TestInvalid8() { Assert.Throws<Exception>(() => { myProxyGenerator.CreateType<IInvalid8>(); }); }
     [Test] public void TestInvalid9() { Assert.Throws<Exception>(() => { myProxyGenerator.CreateType<IInvalid9>(); }); }
