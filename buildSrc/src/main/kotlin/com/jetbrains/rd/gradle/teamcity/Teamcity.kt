b1c75b957c52b125a42c426cae0ce49a605c748e
PavelYatcheniy <pavel.yatcheniy@jetbrains.com>
2019-10-22 01:04:31 +0300

    fix

--- buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/teamcity/Teamcity.kt
+++ buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/teamcity/Teamcity.kt
@@ -3,5 +3,5 @@ package com.jetbrains.rd.gradle.teamcity
 object Teamcity {
     private const val VERSION = "TEAMCITY_VERSION"
 
-    fun onTeamcity() = System.getProperty(VERSION) == null
+    fun onTeamcity() = System.getenv(VERSION) != null
 }
