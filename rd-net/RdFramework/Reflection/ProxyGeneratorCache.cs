c632400fa965079982a75d0c53933ecc79aac125
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2019-10-29 13:04:40 +0100

    fix MethodInfo comparisons

--- rd-net/RdFramework/Reflection/ProxyGeneratorCache.cs
+++ rd-net/RdFramework/Reflection/ProxyGeneratorCache.cs
@@ -11,6 +11,7 @@ namespace JetBrains.Rd.Reflection
     private readonly ProxyGenerator myGenerator;
     private readonly ConcurrentDictionary<Type, Type> myTypesCache = new ConcurrentDictionary<Type, Type>();
     private readonly ConcurrentDictionary<Type, SortedList<MethodInfo, DynamicMethod>> myAdaptersCache = new ConcurrentDictionary<Type, SortedList<MethodInfo, DynamicMethod>>();
+    private static readonly Comparer<MethodInfo> ourMethodComparer = Comparer<MethodInfo>.Create((x,y) => StringComparer.Ordinal.Compare(x.Name, y.Name));
 
     public ProxyGeneratorCache(ProxyGenerator generator)
     {
@@ -24,7 +25,7 @@ namespace JetBrains.Rd.Reflection
 
     public DynamicMethod CreateAdapter(Type selfType, MethodInfo method)
     {
-      var methods = myAdaptersCache.GetOrAdd(selfType, type => new SortedList<MethodInfo, DynamicMethod>());
+      var methods = myAdaptersCache.GetOrAdd(selfType, type => new SortedList<MethodInfo, DynamicMethod>(ourMethodComparer));
       lock (methods)
       {
         if (methods.TryGetValue(method, out var adapter))
