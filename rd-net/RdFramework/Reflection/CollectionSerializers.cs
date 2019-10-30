ad87128e6382aec4b42a2620af6b4886e0509bb5
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2019-10-30 14:57:23 +0100

    review fix

--- rd-net/RdFramework/Reflection/CollectionSerializers.cs
+++ rd-net/RdFramework/Reflection/CollectionSerializers.cs
@@ -1,52 +1,18 @@
 ﻿using System.Collections.Generic;
+using JetBrains.Rd.Impl;
 
 namespace JetBrains.Rd.Reflection
 {
-  public class CollectionSerializers
+  /// <summary>
+  /// Special asymmetric serializers for collections. Used in reflection serializers to use covariant conversion instead of runtime casting.
+  /// </summary>
+  internal class CollectionSerializers
   {
     public static SerializerPair CreateListSerializerPair<T>(SerializerPair itemSerializer)
     {
-      return new SerializerPair(
-        CreateReadListSerializer(itemSerializer.GetReader<T>()),
-        CreateWriteCollectionSerializer(itemSerializer.GetWriter<T>())
-        );
-    }
-
-    public static SerializerPair CreateIListSerializerPair<T>(SerializerPair itemSerializer)
-    {
-      CtxReadDelegate<IList<T>> readListSerializer = CreateReadListSerializer(itemSerializer.GetReader<T>());
-      CtxWriteDelegate<IList<T>> writeListSerializer =(ctx, writer, value) => CreateWriteCollectionSerializer(itemSerializer.GetWriter<T>());
-      return new SerializerPair(
-        readListSerializer,
-        writeListSerializer
-      );
-    }
-
-    public static CtxReadDelegate<List<T>> CreateReadListSerializer<T>(CtxReadDelegate<T> readItem)
-    {
-      return (ctx, reader) =>
-      {
-        var count = reader.ReadInt();
-        var list = new List<T>(count);
-        for (int i = 0; i < count; i++)
-        {
-          list.Add(readItem(ctx, reader));
-        }
-
-        return list;
-      };
-    }
-
-    public static CtxWriteDelegate<ICollection<T>> CreateWriteCollectionSerializer<T>(CtxWriteDelegate<T> writeItem)
-    {
-      return (ctx, writer, value) =>
-      {
-        writer.Write((int)value.Count);
-        foreach (var item in value)
-        {
-          writeItem(ctx, writer, item);
-        }
-      };
+      CtxReadDelegate<IList<T>> readListSerializer = (ctx, reader) => reader.ReadList(itemSerializer.GetReader<T>(), ctx);
+      CtxWriteDelegate<ICollection<T>> writeListSerializer =(ctx, writer, value) => writer.WriteCollection(itemSerializer.GetWriter<T>(), ctx, value);
+      return new SerializerPair(readListSerializer, writeListSerializer);
     }
   }
 }
