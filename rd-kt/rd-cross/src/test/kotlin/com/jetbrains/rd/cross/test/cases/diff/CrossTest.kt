8e868634ff818ceb381d1a25ec9398051edbf615
Dmitry.Ivanov <korifey@gmail.com>
2019-11-19 20:42:37 +0300

    highlight paths to files in CrossTests

--- rd-kt/rd-cross/src/test/kotlin/com/jetbrains/rd/cross/test/cases/diff/CrossTest.kt
+++ rd-kt/rd-cross/src/test/kotlin/com/jetbrains/rd/cross/test/cases/diff/CrossTest.kt
@@ -11,13 +11,15 @@ import java.io.File
 import java.nio.file.Paths
 
 class CrossTest {
+    private fun File.toHighlightablePath() = "file:///"+this.canonicalPath.replace('\\','/')
+
     private fun assertEqualFiles(file1: File, file2: File) {
         val t1 = file1.readText(Charsets.UTF_8)
         val t2 = file2.readText(Charsets.UTF_8)
         if (t1 == t2) {
-            println("The files $file1 and $file2 are same!")
+            println("The files ${file1.toHighlightablePath()} and ${file2.toHighlightablePath()} are same!")
         } else {
-            fail("The files $file1 and $file2 differ!")
+            fail("The files ${file1.toHighlightablePath()} and ${file2.toHighlightablePath()} differ!")
         }
     }
 
@@ -51,7 +53,7 @@ class CrossTest {
     }
 
     @get:Rule
-    public var name = TestName()
+    var name = TestName()
 
     private val methodName get() = name.methodName.replace("test", "")
 
