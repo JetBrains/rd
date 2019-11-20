6dea6774c4d39b39561aa7cf85e238569ce8ef2d
Dmitry.Ivanov <korifey@gmail.com>
2019-11-20 14:06:48 +0300

    Force synchronous scheduler implement TaskScheduler

--- rd-net/Lifetimes/Collections/Viewable/SynchronousScheduler.cs
+++ rd-net/Lifetimes/Collections/Viewable/SynchronousScheduler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using JetBrains.Lifetimes;
 
 namespace JetBrains.Collections.Viewable
 {
-  public class SynchronousScheduler : IScheduler
+  public class SynchronousScheduler : TaskScheduler, IScheduler
   {
-    public static SynchronousScheduler Instance = new SynchronousScheduler();
+    public static readonly SynchronousScheduler Instance = new SynchronousScheduler();
 
     [ThreadStatic] private static int ourActive;
 
@@ -16,11 +18,13 @@ namespace JetBrains.Collections.Viewable
 
     private SynchronousScheduler(){}
 
-    public void Queue(Action action)
+    public void Queue(Action action) => Execute(action);
+
+    private static void Execute(Action action)
     {
       try
       {
-        ourActive ++;
+        ourActive++;
         action();
       }
       finally
@@ -29,7 +33,36 @@ namespace JetBrains.Collections.Viewable
       }
     }
 
-    public bool IsActive { get { return ourActive > 0; } }
-    public bool OutOfOrderExecution { get { return false; } }
+    public bool IsActive => ourActive > 0;
+    public bool OutOfOrderExecution => false;
+
+
+    
+    #region Implementation of TaskScheduler
+    
+    protected override IEnumerable<Task> GetScheduledTasks()
+    {
+      yield break;
+    }
+
+    protected override void QueueTask(Task task)
+    {
+      Execute(() => TryExecuteTask(task));
+    }
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+      try
+      {
+        ourActive++;
+        return TryExecuteTask(task);
+      }
+      finally
+      {
+        ourActive--;
+      }
+    }
+    
+    #endregion
   }
 }
