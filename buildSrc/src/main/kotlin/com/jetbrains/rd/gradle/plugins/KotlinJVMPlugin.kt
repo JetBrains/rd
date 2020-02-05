00fba0712cb308d4924ea287fe5dc53d3c8929dd
operasfantom <operasfantom@mail.ru>
2020-02-05 14:36:38 +0300

    repair junit dependencies

--- buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/plugins/KotlinJVMPlugin.kt
+++ buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/plugins/KotlinJVMPlugin.kt
@@ -91,7 +91,8 @@ open class KotlinJVMPlugin : Plugin<Project> {
                 "implementation"("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
                 "testImplementation"("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-                "testImplementation"("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+                "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+                "testImplementation"("org.junit.jupiter:junit-jupiter-params:$junitVersion")
                 "testImplementation"("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
                 "testImplementation"("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
             }
