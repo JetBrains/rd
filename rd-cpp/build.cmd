12e7b3303f076c63f132b85c5bb20c107a01f978
Alexander <a.g.pirogov@gmail.com>
2020-06-01 19:25:51 +0300

    Switch to VS 2019 toolchain

--- rd-cpp/build.cmd
+++ rd-cpp/build.cmd
@@ -6,7 +6,7 @@ GOTO :CMDSCRIPT
 set -eux
 mkdir -p build
 cd build
-cmake -G "Visual Studio 15 2017" ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
 make
 
 exit 0
@@ -16,7 +16,7 @@ exit 0
 pushd "%~dp0"
 mkdir build
 cd build
-cmake -DENABLE_TESTS_OPTION:BOOL=OFF  -G "Visual Studio 15 2017 Win64"  ..
+cmake -DENABLE_TESTS_OPTION:BOOL=OFF  -G "Visual Studio 16 2019" -A x64  ..
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Debug -j %NUMBER_OF_PROCESSORS%
 cmake -DENABLE_TESTS_OPTION:BOOL=OFF -DBUILD_TYPE=Release -P cmake_install.cmake -j %NUMBER_OF_PROCESSORS%
