7ff84de01dc7d4b8934a7166ec725897d67ca89a
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2020-07-07 18:22:54 +0300

    Fix stackoverflow crash on protocol unbinding * Make RName ref-type to match semantics of C#

--- rd-cpp/src/rd_framework_cpp/src/main/impl/RName.h
+++ rd-cpp/src/rd_framework_cpp/src/main/impl/RName.h
@@ -7,15 +7,13 @@
 
 namespace rd
 {
+class RNameImpl;
+
 /**
  * \brief Recursive name. For constructs like Aaaa.Bbb::CCC
  */
 class RName
 {
-private:
-	RName* parent = nullptr;
-	std::string local_name, separator;
-
 public:
 	// region ctor/dtor
 
@@ -29,16 +27,22 @@ public:
 
 	RName& operator=(RName&& other) noexcept = default;
 
-	RName(RName* parent, string_view localName, string_view separator);
+	RName(RName parent, string_view localName, string_view separator);
 
 	explicit RName(string_view local_name);
 	// endregion
 
 	RName sub(string_view localName, string_view separator);
 
+	explicit operator bool() const
+	{
+		return impl != nullptr;
+	}
+
 	friend std::string to_string(RName const& value);
 
-	static RName Empty();
+private:
+	std::shared_ptr<RNameImpl> impl;
 };
 }	 // namespace rd
 
