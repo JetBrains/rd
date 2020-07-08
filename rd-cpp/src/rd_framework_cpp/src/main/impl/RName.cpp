8df1a5917a323c1a10dc4899edf78bf91355fe2d
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2020-07-08 11:21:40 +0300

    Fix stackoverflow crash on protocol unbinding (#123)
    * Make RName ref-type to match semantics of C#

--- rd-cpp/src/rd_framework_cpp/src/main/impl/RName.cpp
+++ rd-cpp/src/rd_framework_cpp/src/main/impl/RName.cpp
@@ -4,42 +4,65 @@
 
 namespace rd
 {
-RName::RName(RName* parent, string_view localName, string_view separator)
+class RNameImpl
+{
+public:
+	// region ctor/dtor
+	RNameImpl(RName parent, string_view localName, string_view separator);
+
+	RNameImpl(const RName& other) = delete;
+	RNameImpl(RName&& other) noexcept = delete;
+	RNameImpl& operator=(const RNameImpl& other) = delete;
+	RNameImpl& operator=(RNameImpl&& other) noexcept = delete;
+	// endregion
+
+	friend std::string to_string(RNameImpl const& value);
+
+private:
+	RName parent;
+	std::string local_name, separator;
+};
+
+RNameImpl::RNameImpl(RName parent, string_view localName, string_view separator)
 	: parent(parent), local_name(localName), separator(separator)
+{
+}
+
+RName::RName(RName parent, string_view localName, string_view separator)
+	: impl(std::make_shared<RNameImpl>(std::move(parent), localName, separator))
 {
 }
 
 RName RName::sub(string_view localName, string_view separator)
 {
-	return RName(this, localName, separator);
-}
-
-namespace
-{
-RName EMPTY(nullptr, "", "");
+	return RName(*this, localName, separator);
 }
 
 std::string to_string(RName const& value)
 {
-	optional<std::string> res{};
+	std::string res;
+	if (value.impl)
+	{
+		res = to_string(*value.impl);
+	}
+	return res;
+}
+
+std::string to_string(RNameImpl const& value)
+{
 	if (value.parent)
-		res = to_string(*(value.parent));
-	if (res && !res->empty())
 	{
-		return *res + value.separator + value.local_name;
+		std::string res;
+		res = to_string(value.parent);
+		res += value.separator;
+		res += value.local_name;
+		return res;
 	}
-	else
-	{
-		return value.local_name;
-	}
+	return value.local_name;
 }
 
-RName::RName(string_view local_name) : RName(&EMPTY, local_name, "")
+RName::RName(string_view local_name) : RName(RName(), local_name, "")
 {
 }
 
-RName RName::Empty()
-{
-	return EMPTY;
-}
 }	 // namespace rd
