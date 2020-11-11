aff4446ba253116e988d6de389a09bfd96d4cd42
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-11-11 16:38:04 +0100

    SpinWait: implement backoff

--- rd-kt/rd-core/src/jvmMain/kotlin/com/jetbrains/rd/util/threading/SpinWait.kt
+++ rd-kt/rd-core/src/jvmMain/kotlin/com/jetbrains/rd/util/threading/SpinWait.kt
@@ -7,12 +7,16 @@ import java.time.Duration
 
 class SpinWait {
     companion object {
+        @Deprecated("Use static/companion methods")
         val spin by threadLocal { SpinWait() }
 
         inline fun spinUntil(condition: () -> Boolean) {
-            val s = spin
+            var spins = 0L
             while (!condition())
-                s.spinOnce()
+                if(spins++ < 100)
+                    Thread.yield()
+                else
+                    Thread.sleep(spins / 100)
         }
 
         inline fun spinUntil(timeoutMs: Long, condition: () -> Boolean) = spinUntil(Lifetime.Eternal, timeoutMs, condition)
@@ -20,18 +24,22 @@ class SpinWait {
 
         //maybe CancellationException or timeout exception?
         inline fun spinUntil(lifetime: Lifetime, duration: Duration, condition: () -> Boolean) : Boolean {
-            val s = spin
             val start = System.nanoTime()
+            var spins = 0L
             while (!condition()) {
                 if (!lifetime.isAlive || System.nanoTime() - start > duration.toNanos())
                     return false
-                s.spinOnce()
+                if (spins++ < 100)
+                    Thread.yield()
+                else
+                    Thread.sleep(spins / 100)
             }
 
             return true
         }
     }
 
+    @Deprecated("Don't use: state needs to be maintained for proper implementation of backoff")
     fun spinOnce() {
         //todo need to rewrite it with exponential backoff, yield, sleep(0) and sleep(1)
         Thread.yield()
