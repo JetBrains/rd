48ce6e5c04dc536e05dd5112695e18e3356a0878
Ilya.Usov <Ilya.Usov@jetbrains.com>
2021-09-15 15:37:15 +0300

    Fix indentation

--- rd-net/Test.Lifetimes/Collections/BlockingPriorityQueueTest.cs
+++ rd-net/Test.Lifetimes/Collections/BlockingPriorityQueueTest.cs
@@ -1,7 +1,5 @@
-﻿ using System;
- using System.Collections.Generic;
- using System.Diagnostics;
- using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Collections;
