864bf02e75371f053cb46a7d239751ec686bb821
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2021-12-06 11:23:16 +0300

    3rd party license info

--- rd-net/Lifetimes/Annotations/CallerArgumentExpressionAttribute.cs
+++ rd-net/Lifetimes/Annotations/CallerArgumentExpressionAttribute.cs
@@ -1,4 +1,10 @@
-﻿// ReSharper disable once CheckNamespace
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// This was copied from https://github.com/dotnet/runtime/blob/fedf64f5688246b73505537c4dd81fc0afb57ddb/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/CallerArgumentExpressionAttribute.cs
+// and updated to have the scope of the attributes be internal.
+
+// ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
   [AttributeUsage(AttributeTargets.Parameter)]
