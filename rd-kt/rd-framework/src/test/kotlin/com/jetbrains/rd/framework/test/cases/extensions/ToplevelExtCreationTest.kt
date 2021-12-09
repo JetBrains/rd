b394f9a97fa408345ef84b941e6298c9f44feea5
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2021-12-09 22:44:55 +0300

    Remove createExtensionOrThrow method from Protocol

--- rd-kt/rd-framework/src/test/kotlin/com/jetbrains/rd/framework/test/cases/extensions/ToplevelExtCreationTest.kt
+++ rd-kt/rd-framework/src/test/kotlin/com/jetbrains/rd/framework/test/cases/extensions/ToplevelExtCreationTest.kt
@@ -4,20 +4,11 @@ import com.jetbrains.rd.framework.test.util.RdFrameworkTestBase
 import demo.DemoModel
 import demo.demoModel
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertSame
 
 class ToplevelExtCreationTest : RdFrameworkTestBase() {
-    @Test
-    fun testCreateOrThrow() {
-        serverProtocol.createExtensionOrThrow(DemoModel::class) { DemoModel.createModel(serverProtocol.lifetime, serverProtocol) }
-        assertThrows<IllegalStateException> {
-            serverProtocol.createExtensionOrThrow(DemoModel::class) { DemoModel.createModel(serverProtocol.lifetime, serverProtocol) }
-        }
-    }
-    
     @Test
     fun testGetOrNull() {
         assertNull(serverProtocol.tryGetExtension(DemoModel::class))
