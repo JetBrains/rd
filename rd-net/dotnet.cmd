9cacda3dba7351263d01e60975494b12eceec8af
Ivan Migalev <ivan.migalev@jetbrains.com>
2021-12-28 12:19:00 +0700

    rd-net: .NET SDK and language level upgrade

--- rd-net/dotnet.cmd
+++ rd-net/dotnet.cmd
@@ -18,6 +18,16 @@ die () {
     exit 1
 }
 
+function retry_on_error() {
+  local n="$1"
+  shift
+
+  for i in $(seq 2 "$n"); do
+    "$@" 2>&1 && return || echo "WARNING: Command '$1' returned non-zero exit status $?, try again"
+  done
+  "$@"
+}
+
 # OS specific support (must be 'true' or 'false').
 cygwin=false
 msys=false
@@ -41,12 +51,12 @@ DOTNET_TEMP_FILE=$TARGET_DIR/dotnet-sdk-temp.tar.gz
 if [ "$darwin" = "true" ]; then
     case $(uname -m) in
       x86_64)
-        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/56da864b-1efd-46a6-b761-b285283696cb/1ad897dff863fdd3cdbaf8a447b09317/dotnet-sdk-5.0.203-osx-x64.tar.gz
-        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-5.0.203-osx-x64-$SCRIPT_VERSION
+        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/62f78047-71de-460e-85ca-254f1fa848de/ecabeefdca2902f3f06819612cd9d45c/dotnet-sdk-6.0.100-osx-x64.tar.gz
+        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-6.0.100-osx-x64-$SCRIPT_VERSION
         ;;
       arm64)
-        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/e0a3877f-1b66-4e60-81f5-9b5d0769f60d/432bc7949c2d5b15ce218bf62e93520a/dotnet-sdk-6.0.100-preview.3.21202.5-osx-arm64.tar.gz
-        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-6.0.100-preview.3.21202.5-osx-arm64-$SCRIPT_VERSION
+        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/7f1e67c2-11a4-416b-8421-786e47b82fdf/af56581d96e15ed911cf3a172f3c8802/dotnet-sdk-6.0.100-osx-arm64.tar.gz
+        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-6.0.100-osx-arm64-$SCRIPT_VERSION
         ;;
       *)
         echo "Unknown architecture $(uname -m)" >&2; exit 1
@@ -55,13 +65,12 @@ if [ "$darwin" = "true" ]; then
 else
     case $(uname -m) in
       x86_64)
-        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/ef13f9da-46dc-4de9-a05e-5a4c20574189/be95913ebf1fb6c66833ca40060d3f65/dotnet-sdk-5.0.203-linux-x64.tar.gz
-        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-5.0.203-linux-x64-$SCRIPT_VERSION
+        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/17b6759f-1af0-41bc-ab12-209ba0377779/e8d02195dbf1434b940e0f05ae086453/dotnet-sdk-6.0.100-linux-x64.tar.gz
+        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-6.0.100-linux-x64-$SCRIPT_VERSION
         ;;
       aarch64)
-        DOTNET_TEMP_FILE=$TARGET_DIR/dotnet-sdk-5.0.203-linux-arm64.tar.gz
-        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/af5f1e5b-d544-47af-b730-038e4258641b/bccb3982f5690134ab66748a5afc36c7/dotnet-sdk-5.0.203-linux-arm64.tar.gz
-        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-5.0.203-linux-arm64-$SCRIPT_VERSION
+        DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/adcd9310-5072-4179-9b8b-16563b897995/15a7595966f488c74909e4a9273c0e24/dotnet-sdk-6.0.100-linux-arm64.tar.gz
+        DOTNET_TARGET_DIR=$TARGET_DIR/dotnet-sdk-6.0.100-linux-arm64-$SCRIPT_VERSION
         ;;
       *)
         echo "Unknown architecture $(uname -m)" >&2; exit 1
@@ -69,37 +78,70 @@ else
     esac
 fi
 
-set -e
+set -eu
 
-if [ -e "$DOTNET_TARGET_DIR/.flag" ] && [ -n "$(ls "$DOTNET_TARGET_DIR")" ] && [ "x$(cat "$DOTNET_TARGET_DIR/.flag")" = "x${DOTNET_URL}" ]; then
-    # Everything is up-to-date in $DOTNET_TARGET_DIR, do nothing
-    true
+if grep -q -x "$DOTNET_URL" "$DOTNET_TARGET_DIR/.flag" 2>/dev/null; then
+  # Everything is up-to-date in $DOTNET_TARGET_DIR, do nothing
+  true
 else
-  warn "Downloading $DOTNET_URL to $DOTNET_TEMP_FILE"
-
-  rm -f "$DOTNET_TEMP_FILE"
+while true; do  # Note(k15tfu): for goto
   mkdir -p "$TARGET_DIR"
-  if command -v curl >/dev/null 2>&1; then
+
+  LOCK_FILE="$TARGET_DIR/.dotnet-cmd-lock.pid"
+  TMP_LOCK_FILE="$TARGET_DIR/.tmp.$$.pid"
+  echo $$ >"$TMP_LOCK_FILE"
+
+  while ! ln "$TMP_LOCK_FILE" "$LOCK_FILE" 2>/dev/null; do
+    LOCK_OWNER=$(cat "$LOCK_FILE" 2>/dev/null || true)
+    while [ -n "$LOCK_OWNER" ] && ps -p $LOCK_OWNER >/dev/null; do
+      warn "Waiting for the process $LOCK_OWNER to finish bootstrap dotnet.cmd"
+      sleep 1
+      LOCK_OWNER=$(cat "$LOCK_FILE" 2>/dev/null || true)
+
+      # Hurry up, bootstrap is ready..
+      if grep -q -x "$DOTNET_URL" "$DOTNET_TARGET_DIR/.flag" 2>/dev/null; then
+        break 3  # Note(k15tfu): goto out of the outer if-else block.
+      fi
+    done
+
+    if [ -n "$LOCK_OWNER" ] && grep -q -x $LOCK_OWNER "$LOCK_FILE" 2>/dev/null; then
+      die "ERROR: The lock file $LOCK_FILE still exists on disk after the owner process $LOCK_OWNER exited"
+    fi
+  done
+
+  trap "rm -f \"$LOCK_FILE\"" EXIT
+  rm "$TMP_LOCK_FILE"
+
+  if ! grep -q -x "$DOTNET_URL" "$DOTNET_TARGET_DIR/.flag" 2>/dev/null; then
+    warn "Downloading $DOTNET_URL to $DOTNET_TEMP_FILE"
+
+    rm -f "$DOTNET_TEMP_FILE"
+    if command -v curl >/dev/null 2>&1; then
       if [ -t 1 ]; then CURL_PROGRESS="--progress-bar"; else CURL_PROGRESS="--silent --show-error"; fi
-      curl -L $CURL_PROGRESS --output "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
-  elif command -v wget >/dev/null 2>&1; then
+      retry_on_error 5 curl -L $CURL_PROGRESS --output "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
+    elif command -v wget >/dev/null 2>&1; then
       if [ -t 1 ]; then WGET_PROGRESS=""; else WGET_PROGRESS="-nv"; fi
-      wget $WGET_PROGRESS -O "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
-  else
+      retry_on_error 5 wget $WGET_PROGRESS -O "${DOTNET_TEMP_FILE}" "$DOTNET_URL"
+    else
       die "ERROR: Please install wget or curl"
+    fi
+
+    warn "Extracting $DOTNET_TEMP_FILE to $DOTNET_TARGET_DIR"
+    rm -rf "$DOTNET_TARGET_DIR"
+    mkdir -p "$DOTNET_TARGET_DIR"
+
+    tar -x -f "$DOTNET_TEMP_FILE" -C "$DOTNET_TARGET_DIR"
+    rm -f "$DOTNET_TEMP_FILE"
+
+    echo "$DOTNET_URL" >"$DOTNET_TARGET_DIR/.flag"
   fi
 
-  warn "Extracting $DOTNET_TEMP_FILE to $DOTNET_TARGET_DIR"
-  rm -rf "$DOTNET_TARGET_DIR"
-  mkdir -p "$DOTNET_TARGET_DIR"
-
-  tar -x -f "$DOTNET_TEMP_FILE" -C "$DOTNET_TARGET_DIR"
-  rm -f "$DOTNET_TEMP_FILE"
-
-  echo "$DOTNET_URL" >"$DOTNET_TARGET_DIR/.flag"
+  rm "$LOCK_FILE"
+  break
+done
 fi
 
-if [ '!' -e "$DOTNET_TARGET_DIR/dotnet" ]; then
+if [ ! -x "$DOTNET_TARGET_DIR/dotnet" ]; then
   die "Unable to find dotnet under $DOTNET_TARGET_DIR"
 fi
 
@@ -109,16 +151,13 @@ exec "$DOTNET_TARGET_DIR/dotnet" "$@"
 
 setlocal
 set SCRIPT_VERSION=dotnet-cmd-v1
-set COMPANY_NAME=JetBrains
-set TARGET_DIR=%LOCALAPPDATA%\Temp\%COMPANY_NAME%\
-set DOTNET_TARGET_DIR=%TARGET_DIR%dotnet-sdk-5.0.203-win-x64-%SCRIPT_VERSION%\
-set DOTNET_TEMP_FILE=dotnet-sdk-temp.zip
-set DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/46b35cfe-4b3f-4e69-8831-0937196699b1/221f862c003a0175722c131b0941e3c4/dotnet-sdk-5.0.203-win-x64.zip
-
+set COMPANY_NAME=Mikhail Filippov
+set TARGET_DIR=%LOCALAPPDATA%\%COMPANY_NAME%\dotnet-cmd\
+set DOTNET_TARGET_DIR=%TARGET_DIR%dotnet-sdk-6.0.100-win-x64-%SCRIPT_VERSION%\
+set DOTNET_TEMP_FILE=%TARGET_DIR%dotnet-sdk-temp.zip
+set DOTNET_URL=https://cache-redirector.jetbrains.com/download.visualstudio.microsoft.com/download/pr/ca65b248-9750-4c2d-89e6-ef27073d5e95/05c682ca5498bfabc95985a4c72ac635/dotnet-sdk-6.0.100-win-x64.zip
 
 set POWERSHELL=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
-
-if not exist "%DOTNET_TARGET_DIR%" MD "%DOTNET_TARGET_DIR%"
 
 if not exist "%DOTNET_TARGET_DIR%.flag" goto downloadAndExtractDotNet
 
@@ -127,31 +166,39 @@ if "%CURRENT_FLAG%" == "%DOTNET_URL%" goto continueWithDotNet
 
 :downloadAndExtractDotNet
 
-cd /d "%TARGET_DIR%"
-if errorlevel 1 goto fail
+set DOWNLOAD_AND_EXTRACT_DOTNET_PS1= ^
+Set-StrictMode -Version 3.0; ^
+$ErrorActionPreference = 'Stop'; ^
+ ^
+$createdNew = $false; ^
+$lock = New-Object System.Threading.Mutex($true, 'Global\dotnet-cmd-lock', [ref]$createdNew); ^
+if (-not $createdNew) { ^
+    Write-Host 'Waiting for the other process to finish bootstrap dotnet.cmd'; ^
+    [void]$lock.WaitOne(); ^
+} ^
+ ^
+try { ^
+    if ((Get-Content '%DOTNET_TARGET_DIR%.flag' -ErrorAction Ignore) -ne '%DOTNET_URL%') { ^
+        Write-Host 'Downloading %DOTNET_URL% to %DOTNET_TEMP_FILE%'; ^
+        [void](New-Item '%TARGET_DIR%' -ItemType Directory -Force); ^
+        (New-Object Net.WebClient).DownloadFile('%DOTNET_URL%', '%DOTNET_TEMP_FILE%'); ^
+ ^
+        Write-Host 'Extracting %DOTNET_TEMP_FILE% to %DOTNET_TARGET_DIR%'; ^
+        if (Test-Path '%DOTNET_TARGET_DIR%') { ^
+            Remove-Item '%DOTNET_TARGET_DIR%' -Recurse; ^
+        } ^
+        Add-Type -A 'System.IO.Compression.FileSystem'; ^
+        [IO.Compression.ZipFile]::ExtractToDirectory('%DOTNET_TEMP_FILE%', '%DOTNET_TARGET_DIR%'); ^
+        Remove-Item '%DOTNET_TEMP_FILE%'; ^
+ ^
+        Set-Content '%DOTNET_TARGET_DIR%.flag' -Value '%DOTNET_URL%'; ^
+    } ^
+} ^
+finally { ^
+    $lock.ReleaseMutex(); ^
+}
 
-echo Downloading %DOTNET_URL% to %TARGET_DIR%%DOTNET_TEMP_FILE%
-if exist "%DOTNET_TEMP_FILE%" DEL /F "%DOTNET_TEMP_FILE%"
-"%POWERSHELL%" -nologo -noprofile -Command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; (New-Object Net.WebClient).DownloadFile('%DOTNET_URL%', '%DOTNET_TEMP_FILE%')"
-if errorlevel 1 goto fail
-
-rmdir /S /Q "%DOTNET_TARGET_DIR%"
-if errorlevel 1 goto fail
-
-mkdir "%DOTNET_TARGET_DIR%"
-if errorlevel 1 goto fail
-
-cd /d %DOTNET_TARGET_DIR%"
-if errorlevel 1 goto fail
-
-echo Extracting %TARGET_DIR%%DOTNET_TEMP_FILE% to %DOTNET_TARGET_DIR%
-"%POWERSHELL%" -nologo -noprofile -command "Set-StrictMode -Version 3.0; $ErrorActionPreference = \"Stop\"; Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('..\\%DOTNET_TEMP_FILE%', '.');"
-if errorlevel 1 goto fail
-
-del /F "..\%DOTNET_TEMP_FILE%"
-if errorlevel 1 goto fail
-
-echo %DOTNET_URL%>"%DOTNET_TARGET_DIR%.flag"
+"%POWERSHELL%" -nologo -noprofile -Command %DOWNLOAD_AND_EXTRACT_DOTNET_PS1%
 if errorlevel 1 goto fail
 
 :continueWithDotNet
