271f4a6fa68edf4f5ff2b4afe18dfb1148d5e95d
Leonid Mashinskiy <leonid.mashinskiy@jetbrains.com>
2022-05-10 14:57:26 +0300

    rd-cpp: port missed changes from UnrealLink [part 3]

--- rd-cpp/thirdparty/thirdparty.hpp
+++ rd-cpp/thirdparty/thirdparty.hpp
@@ -10,53 +10,56 @@
 #include <variant>
 #include <string_view>
 
-namespace rd {
-	using std::optional;
-	using std::make_optional;
-	using std::nullopt_t;
-	using std::nullopt;
-	using std::variant;
-	using std::get;
-	using std::visit;
-	using std::string_view;
-	using std::wstring_view;
-	using namespace std::literals;
-}
+namespace rd
+{
+using std::get;
+using std::make_optional;
+using std::nullopt;
+using std::nullopt_t;
+using std::optional;
+using std::variant;
+using std::visit;
+}	 // namespace rd
 
 #else
 
 #if defined(_MSC_VER)
-	#pragma warning(push)
-	#pragma warning(disable:4583)
-	#pragma warning(disable:4582)
+#pragma warning(push)
+#pragma warning(disable : 4583)
+#pragma warning(disable : 4582)
 #endif
 #include "optional.hpp"
 #include "mpark/variant.hpp"
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
+namespace rd
+{
+using mpark::get;
+using mpark::variant;
+using mpark::visit;
+using tl::make_optional;
+using tl::nullopt;
+using tl::nullopt_t;
+using tl::optional;
+}	 // namespace rd
+
+#endif
+
 #include "nonstd/string_view.hpp"
-#if defined (_MSC_VER)
-  #pragma warning(pop)
-#endif
+namespace rd
+{
+using nonstd::string_view;
+using nonstd::wstring_view;
+using namespace std::literals;
+using namespace nonstd::literals;
+}	 // namespace rd
 
-namespace rd {
-	using tl::optional;
-	using tl::make_optional;
-	using tl::nullopt_t;
-	using tl::nullopt;
-	using mpark::variant;
-	using mpark::get;
-	using mpark::visit;
-	using nonstd::string_view;
-	using nonstd::wstring_view;
-	using namespace std::literals;
-	using namespace nonstd::literals;
-}
+namespace rd
+{
+using tsl::ordered_map;
+using tsl::ordered_set;
+}	 // namespace rd
 
-#endif
-
-
-namespace rd {
-	using tsl::ordered_set;
-	using tsl::ordered_map;
-}
-
-#endif //RD_CPP_THIRDPARTY_H
+#endif	  // RD_CPP_THIRDPARTY_H
