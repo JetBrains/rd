8f8e6de596a12c7e94c27c6966b9e6e80b1f0b02
ilya.usov <ilyau.usov@jetbrains.com>
2022-07-09 13:22:04 +0200

    do not break task flow if lifetime is terminated

--- rd-net/Lifetimes/Collections/Viewable/SequentialScheduler.cs
+++ rd-net/Lifetimes/Collections/Viewable/SequentialScheduler.cs
@@ -1,57 +1,83 @@
+#if !NET35
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.Threading;
 
 namespace JetBrains.Collections.Viewable
 {
-#if !NET35
+  /// <summary>
+  /// Lightweight task scheduler that executes items sequentially on a passed scheduler
+  /// </summary>
   public sealed class SequentialScheduler : TaskScheduler, IScheduler
   {
+    private readonly bool myAllowInlining;
     private readonly ThreadLocal<uint> myExecutionCount;
     
-    private readonly Lifetime myLifetime;
     private readonly SyncContext mySyncContext;
-    private readonly Actor<Action> myActor;
-    
+    private readonly Channel<Task> myChannel;
+    private readonly ILog myLog;
+
     public bool IsActive => myExecutionCount.Value > 0;
     public bool OutOfOrderExecution => false;
 
-    public static SequentialScheduler FromIScheduler(string id, Lifetime lifetime, IScheduler scheduler)
+    [Obsolete("Use the overload without lifetime")]
+    public static SequentialScheduler FromIScheduler(string id, Lifetime lifetime, IScheduler scheduler) => FromIScheduler(id, scheduler);
+
+    public static SequentialScheduler FromIScheduler(string id, IScheduler scheduler, bool allowInlining = false)
     {
-      return new SequentialScheduler(id, lifetime, scheduler as TaskScheduler ?? new SchedulerWrapper(scheduler));
+      return new SequentialScheduler(id, scheduler as TaskScheduler ?? new SchedulerWrapper(scheduler), allowInlining);
     }
 
-    public SequentialScheduler(string id, Lifetime lifetime, TaskScheduler? scheduler = null)
+    [Obsolete("Use the overload without lifetime")]
+    public SequentialScheduler(string id, Lifetime lifetime, TaskScheduler? scheduler = null) : this(id, scheduler)
     {
-      myLifetime = lifetime;
+    }
+
+    public SequentialScheduler(string id, TaskScheduler? scheduler = null, bool allowInlining = false)
+    {
+      myAllowInlining = allowInlining;
       mySyncContext = new SyncContext(this);
       myExecutionCount = new ThreadLocal<uint>();
-      var def = Lifetime.Define();
-      myActor = new Actor<Action>($"{id} Actor", def.Lifetime, action => action(), scheduler);
-      lifetime.OnTermination(() => myActor.SendBlocking(() => def.Terminate()));
+      myLog = Log.GetLog<SequentialScheduler>().GetSublogger(id);
+      myChannel = Channel.CreateUnbounded<Task>(new UnboundedChannelOptions
+      {
+        SingleReader = true,
+        AllowSynchronousContinuations = false
+      });
+      
+      Lifetime.Eternal.StartAsync(scheduler ?? Default, async () =>
+      {
+        // do not use lifetime here, as this can lead to loss of tasks and consequently to asynchronous deadlocks or breaking tasks flow
+        // in addition, this scheduler is very lightweight, and we can afford not to collect resources manually, and leave everything to the garbage collector
+        while (true)
+        {
+          var task = await myChannel.Reader.ReadAsync();
+          TryExecuteTaskWithContext(task);
+        }
+      }).NoAwait();
     }
 
     public void Queue(Action action)
     {
-      using var cookie = myLifetime.UsingExecuteIfAlive();
-      if (!cookie.Succeed) throw new LifetimeCanceledException(myLifetime);
-      
-      myActor.SendBlocking(() => ExecuteWithContext(action));
+      // use TPL to keep correct context-capture behavior
+      Task.Factory.StartNew(() => myLog.Catch(action), CancellationToken.None, TaskCreationOptions.None, this);
     }
 
-    protected override void QueueTask(Task task) => Queue(() => TryExecuteTask(task));
+    protected override void QueueTask(Task task) => Assertion.Assert(myChannel.Writer.TryWrite(task));
 
-    private void ExecuteWithContext(Action action)
+    private bool TryExecuteTaskWithContext(Task task)
     {
       var old = SynchronizationContext.Current;
       SynchronizationContext.SetSynchronizationContext(mySyncContext);
       myExecutionCount.Value++;
       try
       {
-        action();
+        return TryExecuteTask(task);
       }
       finally
       {
@@ -60,7 +86,11 @@ namespace JetBrains.Collections.Viewable
       }
     }
 
-    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) => false;
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+      return myAllowInlining && IsActive && TryExecuteTaskWithContext(task);
+    }
+
     protected override IEnumerable<Task> GetScheduledTasks() => Array.Empty<Task>();
 
     private class SyncContext : SynchronizationContext
@@ -93,5 +123,5 @@ namespace JetBrains.Collections.Viewable
       protected override IEnumerable<Task> GetScheduledTasks() => Array.Empty<Task>();
     }
   }
-#endif
 }
+#endif
--- rd-net/Test.Lifetimes/Collections/Viewable/SequentialSchedulerTest.cs
+++ rd-net/Test.Lifetimes/Collections/Viewable/SequentialSchedulerTest.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Collections.Viewable;
-using JetBrains.Lifetimes;
+using JetBrains.Util.Internal;
 using NUnit.Framework;
 
 namespace Test.Lifetimes.Collections.Viewable
@@ -24,7 +24,7 @@ namespace Test.Lifetimes.Collections.Viewable
     public void SimpleTest()
     {
       const int n = 1000;
-      var scheduler = CreateSequentialScheduler("TestScheduler", TestLifetime);
+      var scheduler = CreateSequentialScheduler("TestScheduler");
       
       Assert.IsFalse(scheduler.OutOfOrderExecution, "scheduler.OutOfOrderExecution");
       
@@ -34,7 +34,7 @@ namespace Test.Lifetimes.Collections.Viewable
         for (var i = 0; i < 100; i++)
         {
           var sum = 0;
-          var range = Enumerable.Range(0, n);
+          var range = Enumerable.Range(0, n).ToArray();
           var tasks = range.Select(j => TestLifetime.Start(scheduler, () =>
           {
             sum += j;
@@ -76,48 +76,38 @@ namespace Test.Lifetimes.Collections.Viewable
     public void DisposeTest()
     {
       const int n = 1000;
-      var def = TestLifetime.CreateNested();
-      var scheduler = CreateSequentialScheduler("TestScheduler", def.Lifetime);
-      var count = 0;
-      var reached = false;
       
-      def.Lifetime.Start(scheduler, () =>
+      var scheduler = CreateSequentialScheduler("TestScheduler");
+      var count = 0;
+      
+      TestLifetime.Start(scheduler, () =>
       {
         for (var i = 0; i < n; i++)
           scheduler.Queue(() => count++);
         Assert.AreEqual(0, count);
-        def.Terminate();
         Assert.AreEqual(0, count);
-        Assert.Throws<LifetimeCanceledException>(() => scheduler.Queue(() => reached = true));
       }).Wait();
       
-      Assert.AreEqual(LifetimeStatus.Terminated, def.Status);
-      
-      SpinWait.SpinUntil(() => count == n, TimeSpan.FromSeconds(5));
+      SpinWait.SpinUntil(() => Memory.VolatileRead(ref count) == n, TimeSpan.FromSeconds(5));
       
       Assert.AreEqual(n, count);
-      Assert.IsFalse(reached);
-      Assert.Throws<LifetimeCanceledException>(() => scheduler.Queue(() => reached = true));
-      Assert.IsFalse(reached);
     }
     
     [Test]
     public void IsActiveTest()
     {
-      using var def = TestLifetime.CreateNested();
-      
-      var scheduler1 = CreateSequentialScheduler("TestScheduler1", def.Lifetime);
-      var scheduler2 = CreateSequentialScheduler("TestScheduler2", def.Lifetime);
+      var scheduler1 = CreateSequentialScheduler("TestScheduler1");
+      var scheduler2 = CreateSequentialScheduler("TestScheduler2");
       
       Assert.IsFalse(scheduler1.IsActive);
       Assert.IsFalse(scheduler2.IsActive);
       
-      def.Lifetime.Start(scheduler1, () =>
+      TestLifetime.Start(scheduler1, () =>
       {
         Assert.IsTrue(scheduler1.IsActive);
         Assert.IsFalse(scheduler2.IsActive);
         
-        def.Lifetime.Start(scheduler2, () =>
+        TestLifetime.Start(scheduler2, () =>
         {
           Assert.IsFalse(scheduler1.IsActive);
           Assert.IsTrue(scheduler2.IsActive);
@@ -127,12 +117,12 @@ namespace Test.Lifetimes.Collections.Viewable
         Assert.IsFalse(scheduler2.IsActive);
       }).Wait();
       
-      def.Lifetime.StartAsync(scheduler1, async () =>
+      TestLifetime.StartAsync(scheduler1, async () =>
       {
         Assert.IsTrue(scheduler1.IsActive);
         Assert.IsFalse(scheduler2.IsActive);
         
-        await def.Lifetime.Start(scheduler2, () =>
+        await TestLifetime.Start(scheduler2, () =>
         {
           Assert.IsFalse(scheduler1.IsActive);
           Assert.IsTrue(scheduler2.IsActive);
@@ -146,12 +136,12 @@ namespace Test.Lifetimes.Collections.Viewable
       Assert.IsFalse(scheduler2.IsActive);
     }
 
-    private SequentialScheduler CreateSequentialScheduler(string id, Lifetime lifetime)
+    private SequentialScheduler CreateSequentialScheduler(string id)
     {
       return mySchedulerKind switch
       {
-        SchedulerKind.TaskScheduler => new SequentialScheduler(id, lifetime, TaskScheduler.Default),
-        SchedulerKind.ISchduler => SequentialScheduler.FromIScheduler(id, lifetime, DefaultScheduler.Instance),
+        SchedulerKind.TaskScheduler => new SequentialScheduler(id, TaskScheduler.Default),
+        SchedulerKind.ISchduler => SequentialScheduler.FromIScheduler(id, DefaultScheduler.Instance),
         _ => throw new ArgumentOutOfRangeException()
       };
     }
