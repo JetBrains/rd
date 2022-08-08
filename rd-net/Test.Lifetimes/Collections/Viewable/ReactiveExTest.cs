f3e31c3a5c62a146997859eaa0c191973fb57ac3
Ilya Usov <ilya.usov@jetbrains.com>
2022-08-08 18:08:33 +0200

    Add NextValueAsyncTest

---
+++ rd-net/Test.Lifetimes/Collections/Viewable/ReactiveExTest.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using JetBrains.Collections.Viewable;
+using NUnit.Framework;
+
+namespace Test.Lifetimes.Collections.Viewable;
+
+[TestFixture]
+public class ReactiveExTest : LifetimesTestBase
+{
+#if !NET35
+  [Test]
+  public void NextValueAsyncTest()
+  {
+    var lifetimeDef = TestLifetime.CreateNested();
+    var signal = new Signal<int>();
+    {
+      var task = signal.NextValueAsync(lifetimeDef.Lifetime, i => i == 1);
+      Assert.IsFalse(task.IsCompleted);
+    
+      signal.Fire(0);
+      Assert.IsFalse(task.IsCompleted);
+    
+      signal.Fire(1);
+      Assert.AreEqual(TaskStatus.RanToCompletion, task.Status);
+      Assert.AreEqual(1, task.Result);
+    }
+
+    {
+      var task = signal.NextValueAsync(lifetimeDef.Lifetime);
+      lifetimeDef.Terminate();
+      Assert.IsTrue(task.IsCanceled);
+    }
+  }
+#endif
+}
