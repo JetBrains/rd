07e67bad55c1cd6d0ae756d0303fd77e837d47d2
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2022-10-05 10:54:12 +0200

    Diagnostics for missing JET_MODE_ASSERT value

--- rd-net/Lifetimes/Diagnostics/Mode.cs
+++ rd-net/Lifetimes/Diagnostics/Mode.cs
@@ -4,5 +4,21 @@ namespace JetBrains.Diagnostics;
 
 public static class Mode
 {
-  public static readonly bool Assertion = AppDomain.CurrentDomain.GetData("JET_MODE_ASSERT") as bool? == true;
+  /// <summary>
+  /// Whether asserts are enabled. Can be configured by setting JET_MODE_ASSERT = true/false in Current App Domain storage.
+  /// </summary>
+  public static readonly bool Assertion;
+
+  /// <summary>
+  /// True if JET_MODE_ASSERT wasn't specified at the moment of static constructor invocation and default value was
+  /// used instead.
+  /// </summary>
+  public static readonly bool UndefinedAssertion;
+
+  static Mode()
+  {
+    var data = AppDomain.CurrentDomain.GetData("JET_MODE_ASSERT") as bool?;
+    Assertion = data == true;
+    UndefinedAssertion = !data.HasValue;
+  }
 }
