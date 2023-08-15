f3d119c376d412092a970dd2c3319b409f09e4bf
Ilya.Usov <Ilya.Usov@jetbrains.com>
2023-08-15 14:37:49 +0200

    Add asFlow for signal

---
+++ rd-kt/rd-core/src/main/kotlin/com/jetbrains/rd/util/reactive/SignalEx.kt
@@ -0,0 +1,25 @@
+package com.jetbrains.rd.util.reactive
+
+import com.jetbrains.rd.util.lifetime.Lifetime
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.consumeAsFlow
+
+class SignalFlow<T>(private val signal: ISignal<T>) : Flow<T> {
+
+    override suspend fun collect(collector: FlowCollector<T>): Nothing {
+        Lifetime.using { lifetime ->
+            val channel = Channel<T>()
+            signal.advise(lifetime) {
+                channel.trySend(it)
+            }
+
+            channel.consumeAsFlow().collect(collector)
+        }
+
+        error("Unreachable")
+    }
+}
+
+fun <T> ISignal<T>.asFlow() = SignalFlow(this)
