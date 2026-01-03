e9b4f9fca7fecdc77c794fe19b498e6068927e45
Ivan Migalev <ivan.migalev@jetbrains.com>
2026-01-03 19:57:47 +0100

    CommonUtil: migrate to more modern Dokka API

--- buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/plugins/CommonUtil.kt
+++ buildSrc/src/main/kotlin/com/jetbrains/rd/gradle/plugins/CommonUtil.kt
@@ -1,25 +1,27 @@
 package com.jetbrains.rd.gradle.plugins
 
+import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.tasks.bundling.Jar
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.gradle.kotlin.dsl.*
+import org.jetbrains.dokka.gradle.DokkaExtension
 
 fun Project.createPackageJavaDoc(files: ConfigurableFileCollection): Jar {
-    val dokkaHtml by tasks.named<DokkaTask>("dokkaHtml") {
-        outputDirectory.set(file("$buildDir/javadoc"))
-        dokkaSourceSets {
-            named("main") {
-                sourceRoots.from(files)
-                reportUndocumented.set(false)
-            }
+    
+    extensions.configure(DokkaExtension::class.java) {
+        dokkaPublications.named("html") {
+            outputDirectory.set(file("$buildDir/javadoc"))
+        }
 
+        dokkaSourceSets.named("main") {
+            sourceRoots.from(files)
+            reportUndocumented.set(false)
         }
     }
-
+    
     val packageJavadoc by tasks.creating(Jar::class) {
-        dependsOn(dokkaHtml)
+        dependsOn("dokkaGenerateHtml")
         from("$buildDir/javadoc")
         archiveClassifier.set("javadoc")
     }
