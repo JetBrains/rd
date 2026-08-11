JetBrains/rd

commit d63e00288305ec4a453d63590542f004f9e03085

Fix compilation warnings for rd-cpp.

rd-cpp/thirdparty/clsocket/src/SimpleSocket.cpp
@@ -401,7 +401,7 @@ int32_t CSimpleSocket::Send(const uint8_t *pBuf, size_t bytesToSend)
                 //---------------------------------------------------------
                 do
                 {
-                    m_nBytesSent = SEND(m_socket, pBuf, bytesToSend, 0);
+                    m_nBytesSent = static_cast<int32_t>(SEND(m_socket, pBuf, bytesToSend, 0));
                     TranslateSocketError();
                 } while (GetSocketError() == CSimpleSocket::SocketInterrupted);
 
@@ -436,7 +436,7 @@ int32_t CSimpleSocket::Send(const uint8_t *pBuf, size_t bytesToSend)
                 {
                     do
                     {
-                        m_nBytesSent = SENDTO(m_socket, pBuf, bytesToSend, 0, (const sockaddr *)&m_stServerSockaddr, sizeof(m_stServerSockaddr));
+                        m_nBytesSent = static_cast<int32_t>(SENDTO(m_socket, pBuf, bytesToSend, 0, reinterpret_cast<const sockaddr*>(&m_stServerSockaddr), sizeof(m_stServerSockaddr)));
                         TranslateSocketError();
                     } while (GetSocketError() == CSimpleSocket::SocketInterrupted);
                 }
@@ -594,7 +594,7 @@ int32_t CSimpleSocket::Send(const struct iovec *sendVector, int32_t nNumItems)
     SetSocketError(SocketSuccess);
     m_nBytesSent = 0;
 
-    if ((m_nBytesSent = WRITEV(m_socket, sendVector, nNumItems)) == CSimpleSocket::SocketError)
+    if ((m_nBytesSent = static_cast<int32_t>(WRITEV(m_socket, sendVector, nNumItems)) == CSimpleSocket::SocketError))
     {
         TranslateSocketError();
     }
@@ -763,8 +763,7 @@ int32_t CSimpleSocket::Receive(int32_t nMaxBytes, uint8_t * pBuffer )
     {
         do
         {
-            m_nBytesReceived = RECV(m_socket, (pWorkBuffer + m_nBytesReceived),
-                                    nMaxBytes, m_nFlags);
+            m_nBytesReceived = static_cast<int32_t>(RECV(m_socket, (pWorkBuffer + m_nBytesReceived), nMaxBytes, m_nFlags));
             TranslateSocketError();
         } while ((GetSocketError() == CSimpleSocket::SocketInterrupted));
 
@@ -780,8 +779,7 @@ int32_t CSimpleSocket::Receive(int32_t nMaxBytes, uint8_t * pBuffer )
         {
             do
             {
-                m_nBytesReceived = RECVFROM(m_socket, pWorkBuffer, nMaxBytes, 0,
-                                            &m_stMulticastGroup, &srcSize);
+                m_nBytesReceived = static_cast<int32_t>(RECVFROM(m_socket, pWorkBuffer, nMaxBytes, 0, &m_stMulticastGroup, &srcSize));
                 TranslateSocketError();
             } while (GetSocketError() == CSimpleSocket::SocketInterrupted);
         }
@@ -789,8 +787,7 @@ int32_t CSimpleSocket::Receive(int32_t nMaxBytes, uint8_t * pBuffer )
         {
             do
             {
-                m_nBytesReceived = RECVFROM(m_socket, pWorkBuffer, nMaxBytes, 0,
-                                            &m_stClientSockaddr, &srcSize);
+                m_nBytesReceived = static_cast<int32_t>(RECVFROM(m_socket, pWorkBuffer, nMaxBytes, 0, &m_stClientSockaddr, &srcSize));
                 TranslateSocketError();
             } while (GetSocketError() == CSimpleSocket::SocketInterrupted);
         }
@@ -1183,4 +1180,3 @@ bool CSimpleSocket::Select(int32_t nTimeoutSec, int32_t nTimeoutUSec)
 
     return bRetVal;
 }
-

rd-cpp/src/rd_framework_cpp/src/main/base/RdBindableBase.cpp
@@ -14,13 +14,13 @@ bool RdBindableBase::is_bound() const
 	return parent != nullptr;
 }
 
-void RdBindableBase::bind(Lifetime lf, IRdDynamic const* parent, string_view name) const
+void RdBindableBase::bind(Lifetime lf, IRdDynamic const* inParent, string_view name) const
 {
-	RD_ASSERT_MSG(!is_bound(), ("Trying to bind already bound this to " + to_string(parent->get_location())));
+	RD_ASSERT_MSG(!is_bound(), ("Trying to bind already bound this to " + to_string(inParent->get_location())));
 	lf->bracket(
-		[this, lf, parent, &name] {
-			this->parent = parent;
-			location = parent->get_location().sub(name, ".");
+		[this, lf, inParent, &name] {
+			this->parent = inParent;
+			location = inParent->get_location().sub(name, ".");
 			this->bind_lifetime = lf;
 		},
 		[this, lf]() {
@@ -88,13 +88,13 @@ SerializationCtx& RdBindableBase::get_serialization_context() const
 	}
 }
 
-void RdBindableBase::init(Lifetime lifetime) const
+void RdBindableBase::init(Lifetime inLifetime) const
 {
 	for (const auto& it : bindable_extensions)
 	{
 		if (it.second != nullptr)
 		{
-			bindPolymorphic(*(it.second), lifetime, this, it.first);
+			bindPolymorphic(*(it.second), inLifetime, this, it.first);
 		}
 	}
 }

rd-cpp/src/rd_framework_cpp/src/main/base/RdBindableBase.h
@@ -45,7 +45,7 @@ public:
 	// endregion
 
 	// need to implement in subclasses
-	virtual void init(Lifetime lifetime) const;
+	virtual void init(Lifetime inLifetime) const;
 
 	const RName& get_location() const override;
 
@@ -53,7 +53,7 @@ public:
 
 	void set_id(RdId id) const override;
 
-	void bind(Lifetime lf, IRdDynamic const* parent, string_view name) const override;
+	void bind(Lifetime lf, IRdDynamic const* inParent, string_view name) const override;
 
 	void identify(const Identities& identities, RdId const& id) const override;
 

rd-cpp/src/rd_framework_cpp/src/main/protocol/MessageBroker.cpp
@@ -91,11 +91,11 @@ void MessageBroker::dispatch(RdId id, Buffer message) const
 				{
 					auto t = std::move(broker[id]);
 					broker.erase(id);
-					for (auto& it : t.custom_scheduler_messages)
+					for (auto& schedMsg : t.custom_scheduler_messages)
 					{
 						RD_ASSERT_MSG(subscription->get_wire_scheduler() != default_scheduler,
 							"require equals of wire and default schedulers")
-						invoke(subscription, std::move(it));
+						invoke(subscription, std::move(schedMsg));
 					}
 				}
 			};

commit cd3aff919909c2023c5519aa5ff03dfa7b7d247a

Fixed operator precedence in SimpleSocket.cpp
Issue introduced in d63e00288305ec4a453d63590542f004f9e03085

rd-cpp/thirdparty/clsocket/src/SimpleSocket.cpp
@@ -588,7 +588,7 @@ int32_t CSimpleSocket::Send(const struct iovec *sendVector, int32_t nNumItems)
     SetSocketError(SocketSuccess);
     m_nBytesSent = 0;
 
-    if ((m_nBytesSent = static_cast<int32_t>(WRITEV(m_socket, sendVector, nNumItems)) == CSimpleSocket::SocketError))
+    if ((m_nBytesSent = static_cast<int32_t>(WRITEV(m_socket, sendVector, nNumItems))) == CSimpleSocket::SocketError)
     {
         TranslateSocketError();
     }